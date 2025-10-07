64d51ffda120fd883e735dedb8c2177df94fcfdb
yusufammar <yousifammar.ya@gmail.com>
2025-10-07 14:41:33 +0200

    fix: remove exposed (commented) PostgreSQL URI from code

--- backend/config/db.js
+++ backend/config/db.js
@@ -14,8 +14,9 @@ types.setTypeParser(DATE_OID, (val) => val); // Override default parser: keep as
 //     password: process.env.DB_PASSWORD
 
 // })
-
+//----------------------------------
 // For External Connections
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -28,8 +29,15 @@ const pool = new Pool({
   },
 });
 
+// const pool = new Pool({
+// connectionString:""
+//   ssl: {
+//     require: true,
+//     rejectUnauthorized: false,
+//   },
+// });
+
+
 
 
 module.exports = pool;
-
-// postgresql://food_app_user:Wo02nPm7vwsMAMSyn0Bbb8sbA7eAopWQ@dpg-d3hj9ppr0fns73cect70-a.frankfurt-postgres.render.com/food_app_db_9o2k
